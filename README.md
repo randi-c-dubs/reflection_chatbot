d17daeffc032c08cdc98299ecd2a28b264b3b838
randi-c-dubs <randi.w.2012@gmail.com>
2023-02-27 22:33:26 +0000

    Commit of initial files for chatbot app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # reflection_chatbot
 Uses a LLM to help programmers manage their project direction, set goals, and iterate on their work.
+
+# Build Setup
+    # install dependencies
+    npm install
+
+    # start development server at localhost:3000
+    npm start
---
+++ reflection_chatbot/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Chatbot from "react-chatbot-kit";
+
+import ActionProvider from "./chatbot/ActionProvider";
+import MessageParser from "./chatbot/MessageParser";
+import config from "./chatbot/config";
+
+import "./App.css";
+import 'react-chatbot-kit/build/main.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+        <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
