23e4eb44d35199207d1882dbec894a352861914d
randi-c-dubs <randi.w.2012@gmail.com>
2023-02-27 23:16:29 +0000

    Quick test of OpenAI endpoint

--- reflection_chatbot/src/chatbot/MessageParser.js
+++ reflection_chatbot/src/chatbot/MessageParser.js
@@ -5,8 +5,58 @@ class MessageParser {
     this.state = state;
   }
 
-  parse(message) {
+  async parse(message) {
     console.log(message);
+    let response = await this.generateSentence(message);
+    console.log(response);
+  }
+
+  async generateSentence(usrMsg) {
+    const params = {
+      method: "POST",
+      message: "Successfully generated sentence from GPT-3",
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: usrMsg,
+        temperature: 0.7,
+        max_tokens: 96,
+        top_p: 1,
+        n: 3,
+        frequency_penalty: 0,
+        presence_penalty: 0.3,
+      }),
+    };
+    let response = await this.request(params);
+    return response;
+  }
+
+  async request(params) {
+    const options = {
+      method: params.method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
+      },
+    };
+    if (params.body) {
+      options.body = params.body;
+    }
+
+    try {
+      const r = await fetch(`https://api.openai.com/v1/completions`, options);
+      if (!r.ok) {
+        const res = await r.json();
+        throw new Error(res.error);
+      }
+
+      const res = await r.json();
+      if (res.choices.length > 0) {
+        return res.choices[0].text;
+      }
+    } catch (e) {
+      console.log("There was an error fetching suggested sentences");
+      console.log(e);
+    }
   }
 }
 
--- reflection_chatbot/src/chatbot/config.js
+++ reflection_chatbot/src/chatbot/config.js
@@ -2,7 +2,7 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)],
+  initialMessages: [createChatBotMessage(`Welcome to the Reflection Chatbot. What would you like to chat about?`)],
 };
 
 export default config;
