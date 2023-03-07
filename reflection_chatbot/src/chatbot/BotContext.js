7ebc7b7ff2f7227668d0233387d8f1e91ef11026
randi-c-dubs <randi.w.2012@gmail.com>
2023-03-07 23:19:43 +0000

    Starter code for managing the bot's state

---
+++ reflection_chatbot/src/chatbot/BotContext.js
@@ -0,0 +1,14 @@
+const Contexts = {
+      Start: Symbol("start"),
+
+      FeedbackStakeholders: Symbol("stakeholders"),
+      FeedbackHarms: Symbol("harms"),
+      FeedbackBenefits: Symbol("benefits"),
+      FeedbackFeatures: Symbol("fearures"),
+
+      DesignProblemDef: Symbol("problem defintion"),
+      DesignAlgorithmDesign: Symbol("algorithm design"),
+      DesignDeployment: Symbol("project deployment"),
+}
+
+export default Contexts;
