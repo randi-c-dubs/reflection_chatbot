df70115cfc9846907f5c6aa0556b0a102a2a28c9
randi-c-dubs <randi.w.2012@gmail.com>
2023-03-14 17:16:57 +0000

    Putting GPT references in a separate file

--- reflection_chatbot/src/chatbot/ActionProvider.js
+++ reflection_chatbot/src/chatbot/ActionProvider.js
@@ -1,7 +1,7 @@
 import Contexts from "./BotContext";
-//import GPT from "./GPTController"
+import GPT from "../gpt/GPTController"
 
-// ActionProvider code
+// The ActionProvider class controls what the chatbot does and responds
 class ActionProvider {
   constructor(
     createChatBotMessage,
@@ -39,8 +39,8 @@ class ActionProvider {
     this.context = Contexts.Start;
 
     // check for API key
-    //this.clearApiKey(); // for testing API key get
-    this.getApiKey();
+    //GPT.clearApiKey(); // for testing API key get\
+    GPT.getApiKey();
 
     //console.log("Calling ActionProvider constructor");
   }
@@ -50,7 +50,7 @@ class ActionProvider {
     // get feedback or add to design journal
     // update the bot context depending on the category
     // deliver a response
-    this.postChatResponse("let's get started");
+    this.sendBotMessage("let's get started");
     this.updateBotContext(Contexts.FeedbackStakeholders);
   };
 
@@ -72,83 +72,6 @@ class ActionProvider {
       ...prev,
       messages: [...prev.messages, botMessage],
     }));
-  };
-
-  // TODO remove this and put in a separate js file that handles GPT commands
-  getGPTResponse = async (msgLog) => {
-    // make request of gpt
-    // if there is an error (hopefully because of api key issue) will get stuck in a loop
-    let resp;
-    let attempts = 0;
-    while (!resp && attempts < this.apiAttemptLimit) {
-      resp = await this.sendGPTRequest(msgLog);
-      attempts++;
-    }
-    // TODO at attempt limit, display error message and pause chatbot
-
-    if (resp) return resp;
-  };
-
-  sendGPTRequest = async (msgLog) => {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${this.apiKey}`,
-      },
-    };
-    options.body = JSON.stringify({
-      model: "gpt-3.5-turbo", // chatgpt api
-      messages: msgLog,
-      temperature: 0.7,
-      max_tokens: 96,
-      top_p: 1,
-      n: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.3,
-    });
-
-    try {
-      const r = await fetch(
-        `https://api.openai.com/v1/chat/completions`,
-        options
-      );
-      if (!r.ok) {
-        if (r.status === 401) {
-          this.clearApiKey();
-          this.getApiKey();
-        }
-        const res = await r.json();
-        throw new Error(res.error);
-      }
-
-      const res = await r.json();
-      if (res.choices.length > 0) {
-        //console.log(`request success ${res}`);
-        return res;
-      }
-    } catch (e) {
-      console.log("There was an error fetching suggested sentences");
-      console.log(e);
-    }
-  };
-
-  getApiKey = () => {
-    //console.log(`ActionProvider getting API key`);
-    this.apiKey = localStorage.getItem("OPENAI_KEY");
-
-    while (!this.apiKey) {
-      this.apiKey = window.prompt(`Please enter a valid key for OPENAI`);
-      console.log(`got API key ${this.apiKey}`);
-      if (this.apiKey && this.apiKey !== " ") {
-        localStorage.setItem("OPENAI_KEY", this.apiKey);
-      }
-    }
-  };
-
-  clearApiKey = () => {
-    console.log(`clearing stored API key`);
-    localStorage.clear();
   };
 }
 
