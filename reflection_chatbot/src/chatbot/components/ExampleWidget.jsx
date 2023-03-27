4f0d899b9ac38bb1ffe9f74f24a199d9653e0eda
randi-c-dubs <randi.w.2012@gmail.com>
2023-03-27 22:22:05 +0000

    Updating example widget

--- reflection_chatbot/src/chatbot/components/ExampleWidget.jsx
+++ reflection_chatbot/src/chatbot/components/ExampleWidget.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const ExampleWidget = () => {
-  return <img alt="cute gif of Jibo" src="https://s.wsj.net/public/resources/images/OG-AZ338_STERN1_G_20171129130916.gif"></img>;
+  return (
+    <img
+      alt="Cute gif of Jibo"
+      style={{ width: "225px" }}
+      src="https://s.wsj.net/public/resources/images/OG-AZ338_STERN1_G_20171129130916.gif"
+    ></img>
+  );
 };
 
 export default ExampleWidget;
