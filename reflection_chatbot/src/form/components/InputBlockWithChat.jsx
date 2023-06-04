6e2548d274ee5dead947a0bafc328c8b81786214
randi-c-dubs <randi.w.2012@gmail.com>
2023-06-04 02:31:43 +0000

    Renamed input block component

--- reflection_chatbot/src/form/components/InputBlockWithChat.jsx
+++ reflection_chatbot/src/form/components/InputBlockWithChat.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Spinner from "react-bootstrap/Spinner";
 
 import Chatbot from "react-chatbot-kit";
+import { createChatBotMessage } from "react-chatbot-kit";
+
 import ActionProvider from "../../chatbot/ActionProvider";
 import MessageParser from "../../chatbot/MessageParser";
 import config from "../../chatbot/config";
+import Contexts from "../../chatbot/BotContext";
 
 import "react-chatbot-kit/build/main.css";
-import "./InputBlockWithButton.css";
+import "./InputBlockWithChat.css";
 
-const InputBlockWithButton = ({
+const InputBlockWithChat = ({
   inputtype,
   label,
   id,
@@ -17,9 +20,43 @@ const InputBlockWithButton = ({
   onBlur,
   onChange,
 }) => {
-  const [buttonStatus, setButtonStatus] = React.useState("hidden"); // hidden / loading / visible
-  const [messageRead, setMessageRead] = React.useState(true);
+  const [buttonStatus, setButtonStatus] = React.useState("visible"); // hidden / loading / visible
+  //const [messageRead, setMessageRead] = React.useState(true);
   const [botVisible, setBotVisible] = React.useState(false);
+
+  let initialMsg, initialContext, initialMenu;
+  // setup the chatbot with the appropriate initializations
+  switch (id) {
+    case "description":
+      initialMsg = `The project description covers what your project will accomplish and how.`;
+      initialContext = Contexts.Description;
+      initialMenu = [`Tell me more`, `Show an example`];
+      break;
+    case "stakeholders":
+      initialMsg = `Stakeholders are anyone that might be interested in your project and its outcomes.`;
+      initialContext = Contexts.Stakeholders;
+      initialMenu = [`Tell me more`, `Show an example`,`List common stakeholders`];
+      break;
+    case "positiveImpacts":
+      initialMsg = `Positive impacts are ways that your project benefits your stakeholders.`;
+      initialContext = Contexts.PositiveImpacts;
+      initialMenu = [`Tell me more`, `Show an example`,`List common benefits`];
+      break;
+    case "negativeImpacts":
+      initialMsg = `Negative impacts are ways that your projects might (unintentionally!) harm your staekholders or put them at risk.`;
+      initialContext = Contexts.NegativeImpacts;
+      initialMenu = [`Tell me more`, `Show an example`,`List common risks`];
+      break;
+    default:
+      console.error("Got an invalid id name");
+      console.error(id);
+  }
+
+  config.initialMessages = [
+    createChatBotMessage(initialMsg, { widget: "dynamicOptionsMenu" }),
+  ];
+  config.state.context = initialContext;
+  config.state.menuOptions = initialMenu;
 
   // TODO make this a debounced onchange instead
   const onBlurHandle = (e) => {
@@ -35,7 +72,7 @@ const InputBlockWithButton = ({
     }
 
     // call parent on blur to handle data
-    onBlur(e);
+    onBlur(e.target.id, e.target.value);
   };
   const onChangeHandle = (e) => {
     setButtonStatus("loading");
@@ -113,7 +150,7 @@ const InputBlockWithButton = ({
         }[buttonStatus]
       }
       {
-        /* Chatbot component or static hint box */
+        /* Chatbot component */
         <div style={{ display: botVisible ? "block" : "none" }}>
           <Chatbot
             config={config}
@@ -125,4 +162,4 @@ const InputBlockWithButton = ({
     </div>
   );
 };
-export default InputBlockWithButton;
+export default InputBlockWithChat;
