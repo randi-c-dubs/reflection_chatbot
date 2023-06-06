a07c436ebcb0a22d75b63a57d4447a625d6ba4cd
randi-c-dubs <randi.w.2012@gmail.com>
2023-06-06 23:50:30 +0000

    Use KnowledgeBase in InputBlockWithChat component

--- reflection_chatbot/src/form/components/InputBlockWithChat.jsx
+++ reflection_chatbot/src/form/components/InputBlockWithChat.jsx
@@ -9,7 +9,7 @@ import debounce from "lodash.debounce";
 import ActionProvider from "../../chatbot/ActionProvider";
 import MessageParser from "../../chatbot/MessageParser";
 import config from "../../chatbot/config";
-import Contexts from "../../chatbot/knowledge_base/BotContext";
+import KnowledgeBase from "../../chatbot/resources/KnowledgeBase";
 
 import "react-chatbot-kit/build/main.css";
 import "./InputBlockWithChat.css";
@@ -27,34 +27,13 @@ const InputBlockWithChat = ({
 
   let initialMsg, initialContext, initialMenu;
   // setup the chatbot with the appropriate initializations
-  switch (id) {
-    case "description":
-      initialMsg = `The project description covers what your project will accomplish and how.`;
-      initialContext = Contexts.Description;
-      initialMenu = [`Tell me more`, `Show an example`];
-      break;
-    case "stakeholders":
-      initialMsg = `Stakeholders are anyone that might be interested in your project and its outcomes.`;
-      initialContext = Contexts.Stakeholders;
-      initialMenu = [
-        `Tell me more`,
-        `Show an example`,
-        `List common stakeholders`,
-      ];
-      break;
-    case "positiveImpacts":
-      initialMsg = `Positive impacts are ways that your project benefits your stakeholders.`;
-      initialContext = Contexts.PositiveImpacts;
-      initialMenu = [`Tell me more`, `Show an example`, `List common benefits`];
-      break;
-    case "negativeImpacts":
-      initialMsg = `Negative impacts are ways that your projects might (unintentionally!) harm your stakeholders or put them at risk.`;
-      initialContext = Contexts.NegativeImpacts;
-      initialMenu = [`Tell me more`, `Show an example`, `List common risks`];
-      break;
-    default:
-      console.error("Got an invalid id name");
-      console.error(id);
+  if (id in KnowledgeBase) {
+    initialMsg = KnowledgeBase[id].initialChatMessage;
+    initialContext = KnowledgeBase[id].initialChatContext;
+    initialMenu = KnowledgeBase[id].initialChatMenu;
+  } else {
+    console.error("Got an invalid id on input block");
+    console.error(id);
   }
 
   config.initialMessages = [
